8716cc9d489f201376d743a0ffb2efee8bcce1c4
Varas27 <ramivaras27@gmail.com>
2021-06-22 19:39:42 -0300

    fifth commit

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { HiOutlinePlusCircle } from 'react-icons/hi';
 import { HiOutlineMinusCircle } from 'react-icons/hi';
 
-export const ItemCountComponent = ({initial, stock}) => {
+export const ItemCountComponent = ({initial, stock, onAdd}) => {
     const [counter, setCounter] = useState(1);
 
     const increment = () => {
@@ -20,19 +20,22 @@ export const ItemCountComponent = ({initial, stock}) => {
 
     return(
         <div className="container">
-            <div className="row justify-content-center">
+            <div className="row justify-content-center mt-4">
                 <div className="card col-9 col-md-5 col-lg-4" id="card-counter">
                     <div className="card-body">
                         <h5 className="card-title">Cantidad</h5>
-                            <div className="row mt-4" id="counter">
+                            <div className="row mx-0 mt-4" id="counter">
                                 <button className="btn p-0 mr-auto shadow-none" type="button" onClick={decrement}>
                                     <HiOutlineMinusCircle size="30" color={counter === initial ? '#C2E7F5' : '#2294C6'}/>
                                 </button>
-                                <p className="m-0 mx-4 align-self-center">{counter}</p>
+                                <p className="m-0 mx-4 align-self-center">{stock > 0 ? counter : stock}</p>
                                 <button className="btn p-0 ml-auto shadow-none" type="button" onClick={increment}>
-                                    <HiOutlinePlusCircle size="30" color={counter === stock ? '#C2E7F5' : '#2294C6'} />
+                                    <HiOutlinePlusCircle size="30" color={counter === stock || stock === 0 ? '#C2E7F5' : '#2294C6'} />
                                 </button>
                             </div>
+                        <button className="col-12 btn btn-primary mt-4" type="button" id={stock !== 0 ? 'addToCart' : 'addToCartNoStock'} onClick={stock !== 0 ? () => onAdd(counter) : ''}>
+                            <p className="m-0">Agregar al carrito</p>
+                        </button>
                     </div>
                 </div>
             </div>
--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,10 +1,21 @@
 import { ItemCountComponent } from './../../components/ItemCount/';
 
 export const ItemListContainer = ({greeting}) => {
+    const onAdd = (counter) => {
+        let m;
+        if (counter === 1) {
+            m = `Se agregó ${counter} producto.`
+        } else {
+            m = `Se agregaron ${counter} productos.`
+        };
+
+        alert(m);
+    }
     return (
         <>
             <p>{greeting}</p>
-            <ItemCountComponent initial={1} stock={7} />
+            <ItemCountComponent initial={1} stock={7} onAdd={onAdd}/>
+            <ItemCountComponent initial={1} stock={0} onAdd={onAdd}/>  {/* test stock 0*/}
         </>
     )
 }
